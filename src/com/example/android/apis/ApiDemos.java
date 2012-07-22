066537d5dc79acca3b2e04d6c242b6f898a2d2ce
sato <sato@sato>
2012-07-22 20:15:11 +0900

    test2

--- src/com/example/android/apis/ApiDemos.java
+++ src/com/example/android/apis/ApiDemos.java
@@ -35,7 +35,7 @@ import java.util.Map;
 
 public class ApiDemos extends ListActivity {
 
-	/**/
+	/*git test*/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
