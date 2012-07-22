ed25bcb336e808a035cf4d364812ae45ab3e5aeb
sato <sato@sato>
2012-07-22 21:56:50 +0900

    commit test sec

--- src/com/example/android/apis/ApiDemos.java
+++ src/com/example/android/apis/ApiDemos.java
@@ -35,7 +35,6 @@ import java.util.Map;
 
 public class ApiDemos extends ListActivity {
 
-	/*git test mod aaa */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -143,7 +142,4 @@ public class ApiDemos extends ListActivity {
         Intent intent = (Intent) map.get("intent");
         startActivity(intent);
     }
-
-    // EGit Test Add
-
 }
